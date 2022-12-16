dc4158a0c87b244f5aea78d36a7727660dca5f78
ben-w-splinter <beatboxbeastz80@gmail.com>
2022-12-16 15:16:50 +0000

    Added ability to switch stations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { AudioPlayer } from './components/AudioPlayer';
 import { useRef, useState } from 'react';
-import { AudioControls } from './components/AudioControls';
+import { FaStepBackward, FaPlayCircle, FaStepForward, FaPauseCircle } from 'react-icons/fa'
 
 function App() {
   //Create a list of stations with their titles and ID's
@@ -13,10 +13,50 @@ function App() {
   ];
   //Keeps track of current track
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  //Keeps track if audio is playing
+  const [playing, setPlaying] = useState<boolean>(false);
+
   //Station URL
   const stationURL = useRef("");
   //Generate a link from the stations
   stationURL.current = generateLink(stations[currentTrackIndex].id);
+
+  //Function to skip to the previous track
+  function prevTrack(){
+    //If at the beginning, wrap around
+    if (currentTrackIndex -1 < 0){
+        setCurrentTrackIndex(stations.length - 1);
+    }
+    else{
+        //Set the track index to the previous track
+        setCurrentTrackIndex((value) => value - 1);
+    }
+
+    //Change the station URL
+    stationURL.current = generateLink(stations[currentTrackIndex].id);
+    //Make the playing match the state of the audio
+    setPlaying(false)
+  }
+
+  //Toggle playing
+  function playPause(){
+      setPlaying(!playing);
+  }
+  function nextTrack(){
+      //If at the end, wrap around
+      if (currentTrackIndex +1 === stations.length){
+          setCurrentTrackIndex(0);
+      }
+      else{
+          //Set the track index to the next track
+          setCurrentTrackIndex((value) => value + 1);
+      }
+      
+      //Change the station URL
+      stationURL.current = generateLink(stations[currentTrackIndex].id);
+      //Make the playing match the state of the audio
+      setPlaying(false)
+  }
 
   function generateLink(id: String){
     return "http://listen.181fm.com/181-" + id + "_128k.mp3";
@@ -25,8 +65,12 @@ function App() {
 
   return (
     <div className="App">
-      <AudioPlayer stationURL={stationURL.current}/>
-      <AudioControls/>
+      <AudioPlayer stationURL={stationURL.current} playing = {playing}/>
+      <div className='audio-controls'>
+            <FaStepBackward onClick={prevTrack} tabIndex={1}/>
+            {playing ? <FaPauseCircle tabIndex={2} onClick={playPause}/> : <FaPlayCircle tabIndex={2} onClick= {playPause}/>}
+            <FaStepForward tabIndex={3} onClick={nextTrack}/> 
+        </div>
     </div>
   );
 }
--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useRef } from "react"
+
+
 interface props{
     stationURL : string
+    playing: boolean
 }
 
-export const AudioPlayer = ({stationURL} : props) => {   
+export const AudioPlayer = ({stationURL, playing} : props) => {
+    const audio = useRef<HTMLAudioElement>(null);
+    
+    useEffect(() => {
+        const currentAudio = audio.current
+        if (playing)
+        {  
+          currentAudio?.play();
+        }
+        else
+        {
+            currentAudio?.pause();
+        }
+    }, [audio, playing])
+    
+
     return (
-        <audio controls src={stationURL}/>
+        <audio controls src={stationURL} ref = {audio}/>
     )
 }
